b11dcd17c97df7863846cfb008870b24ba4efd7e
HarshitSinghai <110110533+HarshitSinghai@users.noreply.github.com>
2024-01-26 08:02:10 +0530

    add routes setup for register, logout, getUserMainPage

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,36 @@
+import asyncHandler from 'express-async-handler';
+
 // @desc    Auth user/ get token
 // route    POST /api/users/auth
 // @access  Public
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Auth User'});
-}
+});
 
-export {authUser};
+// @desc    Register a user
+// route    POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'Register User'});
+});
+
+// @desc    Logout user
+// route    POST /api/users/logout
+// @access  Public
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'Logout User'});
+});
+
+// @desc    Get user main page
+// route    GET /api/users/main
+// @access  Private
+const getUserMainPage = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'Main Page'});
+});
+
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserMainPage
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,10 +3,18 @@
 import express from "express";
 const router = express.Router();
 
-import { authUser } from "../controllers/userController.js"; // when you use the import syntax, you are importing your own js file, so you need to add the .js extension otherwise it will give a module not found error.
+import { 
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserMainPage
+} from "../controllers/userController.js"; // when you use the import syntax, you are importing your own js file, so you need to add the .js extension otherwise it will give a module not found error.
 
 
 router.post('/auth', authUser); // final route will be /api/users/auth
+router.post('/', registerUser);
+router.post('/logout', logoutUser);
+router.get('/main')
 
 
 export default router;
